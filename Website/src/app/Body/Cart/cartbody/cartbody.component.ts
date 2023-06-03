a54a7e419081cf64e2384f1830d18768bb944615
Prashant Naiker <82345195+prashant44773@users.noreply.github.com>
2023-06-04 03:21:59 +0530

    Added Images in Kendo Grid

--- Website/src/app/Body/Cart/cartbody/cartbody.component.ts
+++ Website/src/app/Body/Cart/cartbody/cartbody.component.ts
@@ -7,10 +7,12 @@ import { GridComponent } from '@progress/kendo-angular-grid';
 })
 export class CartbodyComponent {
 
-  userTestStatus: { id: number, name: string }[] = [
-    { "id": 0, "name": "Available" },
-    { "id": 1, "name": "Ready" },
-    { "id": 2, "name": "Started" }
+
+
+  userTestStatus: { id: number, name: string ,pro:string}[] = [
+    { "id": 0, "name": "Available" ,pro:"ProductImg1.png"},
+    { "id": 1, "name": "Ready" ,pro:"ProductImg1.png"},
+    { "id": 2, "name": "Started" ,pro:"ProductImg1.png"}
   ];
 
 }
